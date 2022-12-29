c9b95ce3f70831a0b8c0825ae7f0417f907cd401
Margarita Timchenko <miss.margaretti@gmail.com>
2022-12-29 16:29:47 +0200

    sql

---
+++ lesson 16/dz 16.sql
@@ -0,0 +1,5 @@
+select * from employees order by FIRST_NAME asc;
+select first_name, last_name, salary, commission_pct from employees;
+select sum(salary) from employees;
+select max(salary), min(salary) from employees;
+select avg (salary) from employees;
